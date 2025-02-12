bf6caa1db0a4087f19ae81bfe23e17367be45392
Juan Abril <jdac149@gmail.com>
2025-02-12 08:49:24 -0500

    Test secrets

--- src/peticiones/crearPersona.js
+++ src/peticiones/crearPersona.js
@@ -1,16 +1,15 @@
 export const postPersona = async (persona) => {
-    const url = ' http://174.138.127.244/backend/api/personas/crearPesona';
-    console.log('Agregando cliente', persona);
-    const resp = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(persona)
-    });
-  
-    if (resp.ok) {
-      console.log('El cliente se ha agregado correctamente');
-    } else {
-      console.error('Error al agregar el cliente');
-    }
-  };
-    
+  const url = " http://174.138.127.244/backend/api/personas/crearPesona";
+  console.log("Agregando cliente", persona);
+  const resp = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(persona),
+  });
+
+  if (resp.ok) {
+    console.log("El cliente se ha agregado correctamente");
+  } else {
+    console.error("Error al agregar el cliente");
+  }
+};
